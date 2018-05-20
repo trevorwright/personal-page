adc532e2ccd98db00aec84e521d5298d4145d908
Trevor Wright <trevor.wright@prodigygame.com>
2018-05-20 12:44:01 -0400

    Adds font-awesome

--- static.config.js
+++ static.config.js
@@ -13,24 +13,24 @@ export default {
         path: '/',
         component: 'src/containers/Home',
       },
-      {
-        path: '/about',
-        component: 'src/containers/About',
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
+      // {
+      //   path: '/about',
+      //   component: 'src/containers/About',
+      // },
+      // {
+      //   path: '/blog',
+      //   component: 'src/containers/Blog',
+      //   getData: () => ({
+      //     posts,
+      //   }),
+      //   children: posts.map(post => ({
+      //     path: `/post/${post.id}`,
+      //     component: 'src/containers/Post',
+      //     getData: () => ({
+      //       post,
+      //     }),
+      //   })),
+      // },
       {
         is404: true,
         component: 'src/containers/404',
