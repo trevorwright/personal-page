0c7a21306486e56908b757524c5bfcace69afeb4
Trevor Wright <trevor.wright@prodigygame.com>
2018-05-21 11:59:44 -0400

    Adds content to the about section

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { ImageBlurb } from '../../components/common'
 
 import trevorImage from '../../../assets/images/trevor-wright.png'
@@ -7,10 +6,20 @@ import trevorImage from '../../../assets/images/trevor-wright.png'
 const About = () => (
   <ImageBlurb image={trevorImage} alt="Profile Image of Trevor Wright">
     <p>
-      Lorem ipsum dolor sit amet consectetur adipiscing elit, id fusce morbi aliquam ultrices nisi
-      venenatis, proin potenti odio tincidunt tempus volutpat. Mus amet aliquet vehicula lacinia
-      penatibus dui rhoncus nisi nostra viverra taciti adipiscing, id fermentum sem faucibus
-      facilisi maecenas inceptos euismod platea urna porttitor class.
+      I have been developing modern software solutions for the last decade on multiple platforms
+      using a wide variety of technologies. Mentoring and leadership play a huge part in my career,
+      helping team members fine tune and advance their skillset. Learning is a massive passion as
+      there is always a new approach, point of view, or technology to improve the work you do.
+    </p>
+    <p>
+      I wan to continually push our industry forward by enabling teams to do amazing work more
+      efficiently than ever and find new solutions for the problems we face in the industry. The
+      world of web technologies is rapidly evolving and I strive to stay on the cutting edge of it.
+    </p>
+    <p>
+      Outside of work I love spending time with my beautiful girlfriend, embarking on any adventure
+      we can find together. I enjoy the Canadian summers on the golf course, and the winters on the
+      ski slopes. I also love cooking as a hobby and as a means of staying nourished.
     </p>
   </ImageBlurb>
 )
