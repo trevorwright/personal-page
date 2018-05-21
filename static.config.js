5e3fd74a9775001b64880d833601be1170995184
Trevor Wright <trevor.wright@prodigygame.com>
2018-05-21 13:40:39 -0400

    Adds favicons

--- static.config.js
+++ static.config.js
@@ -55,6 +55,10 @@ export default {
           <Head>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+            <title>Trevor Wright | Full Stack Web Developer</title>
             {renderMeta.styleTags}
           </Head>
           <Body>{children}</Body>
